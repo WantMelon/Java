15c14d936dd12baf5587abfa1f90cb846cc44cab
WantMelon <artem.samorosenko31@gmail.com>
2021-04-10 22:01:15 +0300

    Add task4 tests

--- laba3/src/test/MainTest.java
+++ laba3/src/test/MainTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test;
 import task1.FirstComparator;
 import task1.SecondComparator;
+import task4.sorts.BubbleSort;
+import task4.sorts.GnomeSort;
 
 import java.util.Arrays;
 
@@ -36,6 +38,28 @@ class MainTest {
         assertEquals(
                 "one two three, four a5a six 789 zero",
                 task2.Main.parseString(string)
+        );
+    }
+
+    @Test
+    void bubbleSortTest() {
+        String[] actual = {"string", "test", "first", "кто тут?"};
+        String[] expected = {"string", "test", "first", "кто тут?"};
+        new BubbleSort<String>(actual).sort(new FirstComparator("t"));
+        assertArrayEquals(
+                expected,
+                actual
+        );
+    }
+
+    @Test
+    void gnomeSortTest() {
+        String[] actual = {"favail", "sssss", "agufadodolas", "lasfa"};
+        String[] expected = {"agufadodolas", "favail", "lasfa", "sssss"};
+        new GnomeSort<String>(actual).sort(new SecondComparator("fa", "l"));
+        assertArrayEquals(
+                expected,
+                actual
         );
     }
 }
