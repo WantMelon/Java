74e9bd06d2a31453211ec334a193459cc7173efd
WantMelon <artem.samorosenko31@gmail.com>
2021-04-10 15:28:15 +0300

    Fix return value of comparators

--- laba3/src/task1/FirstComparator.java
+++ laba3/src/task1/FirstComparator.java
@@ -3,16 +3,25 @@ package task1;
 import java.util.Comparator;
 
 public class FirstComparator implements Comparator<String> {
+    private int compareCounter;
     private final String substring;
 
     public FirstComparator(String substring) {
+        compareCounter = 0;
         this.substring = substring;
+    }
+
+    public int getCompareCounter() {
+        return compareCounter;
     }
 
     @Override
     public int compare(String s1, String s2) {
+        compareCounter++;
+
         int n1 = s1.lastIndexOf(substring);
         int n2 = s2.lastIndexOf(substring);
-        return Integer.compare(n1, n2);
+
+        return (n1 == -1) ? 1 : ((n2 == -1) ? -1 : Integer.compare(n1, n2));
     }
 }
--- laba3/src/task1/SecondComparator.java
+++ laba3/src/task1/SecondComparator.java
@@ -3,16 +3,20 @@ package task1;
 import java.util.Comparator;
 
 public class SecondComparator implements Comparator<String> {
+    private int compareCounter;
     private final String startSubstring;
     private final String endSubstring;
 
     public SecondComparator(String startSubstring, String endSubstring) {
+        compareCounter = 0;
         this.startSubstring = startSubstring;
         this.endSubstring = endSubstring;
     }
 
     @Override
     public int compare(String s1, String s2) {
+        compareCounter++;
+
         int index1 = s1.indexOf(startSubstring);
         int index2 = s1.lastIndexOf(endSubstring);
         if (index1 == -1 || index2 == -1 || index1 > index2) {
@@ -26,10 +30,6 @@ public class SecondComparator implements Comparator<String> {
             return -1;
         }
         s2 = s2.substring(index1, index2);
-//        String substringS1 = s1.substring(s1.indexOf(startSubstring),
-//                s1.lastIndexOf(endSubstring));
-//        String substringS2 = s2.substring(s2.indexOf(startSubstring),
-//                s2.lastIndexOf(endSubstring));
 
         return s1.compareToIgnoreCase(s2);
     }
