3a1eb45b8ec8b00f3267b29fef256bafb38c073b
WantMelon <artem.samorosenko31@gmail.com>
2021-04-26 22:41:24 +0300

    Add tests

---
+++ laba4/src/test/MainTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.Test;
+import task1.Queue;
+import task1.Stack;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void queueTest() {
+        Queue<Integer> queue = new Queue<>();
+        queue.push(10);
+        queue.push(20);
+
+        assertEquals(
+                10,
+                queue.pop()
+        );
+        assertEquals(
+                20,
+                queue.pop()
+        );
+    }
+
+    @Test
+    void stackTest() {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(2);
+        stack.push(10);
+        assertEquals(
+                10,
+                stack.pop()
+        );
+        assertEquals(
+                2,
+                stack.peek()
+        );
+        assertEquals(
+                1,
+                stack.size()
+        );
+    }
+}
